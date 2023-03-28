451a1eeaf01c9af093bb2f610987172731bfd2c7
Arshad <erpsupport@techboterp.com>
2023-03-28 12:25:24 +0530

    [ADD] Product Barcode Customisation

---
+++ techbot_barcode_customisation/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Barcode Customisation',
+    'version': '16.0.1.1.0',
+    'category': 'Barcode',
+    'summary': """""",
+    'author': 'Techbot',
+    'depends': ['stock'],
+    'data': [
+        'reports/report.xml',
+        'reports/barcode_template.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
---
+++ techbot_barcode_customisation/models/product_label_layout.py
@@ -0,0 +1,67 @@
+from odoo import api, fields, models,_
+from odoo.exceptions import UserError
+
+
+class ProductLabelLayout(models.TransientModel):
+    _inherit = 'product.label.layout'
+
+    print_format = fields.Selection(selection_add=[('10x100', '10mm x 100 mm'), ('zpl',)],ondelete={'10x100': 'set default'})
+
+    def _get_report_data(self):
+        product_list = []
+        if self.move_line_ids:
+            for rec in self.move_line_ids:
+                if self.picking_quantity == 'custom':
+                    qty_done = 1
+                else:
+                    qty_done = rec.qty_done
+                display_name = rec.product_id.name
+                company_name = self.env.company.name
+                telephone = self.env.company.phone
+                barcode = rec.product_id.barcode
+                product_list.append({'display_name': display_name,
+                                     'company_name': company_name,
+                                     'telephone': telephone,
+                                     'qty_done': qty_done,
+                                     'barcode': barcode, })
+        elif self.product_ids:
+            for rec in self.product_ids:
+                display_name = rec.name
+                company_name = self.env.company.name
+                telephone = self.env.company.phone
+                barcode = rec.barcode
+                product_list.append({'display_name': display_name,
+                                     'company_name': company_name,
+                                     'telephone': telephone,
+                                     'qty_done': 1,
+                                     'barcode': barcode, })
+
+        elif self.product_tmpl_ids:
+            for rec in self.product_tmpl_ids:
+                display_name = rec.name
+                company_name = self.env.company.name
+                telephone = self.env.company.phone
+                barcode = rec.barcode
+                product_list.append({'display_name': display_name,
+                                     'company_name': company_name,
+                                     'telephone': telephone,
+                                     'qty_done': 1,
+                                     'barcode': barcode, })
+
+        return {'count':self.custom_quantity,'product_list':product_list}
+
+
+    def process(self):
+        self.ensure_one()
+        xml_id, data = self._prepare_report_data()
+        if self.print_format == '10x100':
+            data = self._get_report_data()
+            xml_id = 'techbot_barcode_customisation.custom_barcode_label_print'
+        if not xml_id:
+            raise UserError(_('Unable to find report template for %s format', self.print_format))
+
+        report_action = self.env.ref(xml_id).report_action(None, data=data)
+        report_action.update({'close_on_report_download': False})
+        return report_action
+
+
