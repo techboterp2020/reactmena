7ef6a31e325accc7760204ff018ae6f46f3bf339
Arshad <arshadzaynuddin@gmail.com>
2023-06-27 14:25:32 +0530

    [UPDT] Product Barcode

---
+++ techbot_company_customisation/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Company Customisation',
+    'version': '16.0.1.1.0',
+    'category': 'Base',
+    'summary': """""",
+    'author': 'Techbot',
+    'depends': ['base'],
+    'data': [
+        'views/res_company.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
